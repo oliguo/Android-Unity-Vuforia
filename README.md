79c0996c0f2fe7f073b65c54d60d0b8e9553099e
oliverguo <oliverguo@outlook.com>
2018-01-20 19:56:49 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# android-vuforia
-Getting Started.
+#android-unity-vuforia
+Study vuforia with unity for android development.
+
+Youtube Video demo
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/uYDDgumH8Xs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+Thanks for reference link:
+
+[Vuforia and Unity教學](https://www.youtube.com/watch?v=bBBX4IWO-TY)
+
+[Unity Vuforia Tutorial - Multiple Image Target
+](https://www.youtube.com/watch?v=QsbvBaJdCIY)
+
+[Augmented Reality - How To Import Animated 3D Object
+](https://www.youtube.com/watch?v=FiCJDJa5Wbs)
+
+[android-3D-model-viewer](https://github.com/andresoviedo/android-3D-model-viewer)
---
+++ Vuforia_Demo/Android/Vuforia_Demo/src/main/java/demo/unity/vuforia/MainActivity.java
@@ -0,0 +1,30 @@
+package demo.unity.vuforia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,UnityPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
